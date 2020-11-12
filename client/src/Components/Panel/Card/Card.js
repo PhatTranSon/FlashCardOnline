f1890b43e332d4028b6ab1dd107aca68147bc70a
PhatTranSon <phatranson1@gmail.com>
2020-11-12 02:38:04 -0800

    Added speaker icon to card

--- client/src/Components/Panel/Card/Card.js
+++ client/src/Components/Panel/Card/Card.js
@@ -3,10 +3,17 @@ import './style.css';
 
 import { formatColor } from '../../../Common/Helpers';
 import Like from '../Collection/Like';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVolumeUp } from '@fortawesome/free-solid-svg-icons';
 
 class Card extends React.Component {
     constructor(props) {
         super(props);
+
+        //State
+        this.state = {
+            hover: false
+        }
 
         //Bind methods
         this.onDelete = this.onDelete.bind(this);
@@ -29,16 +36,20 @@ class Card extends React.Component {
             <div 
                 className="flashcard card"
                 style={{ background: formatColor(color) }}>
-                <h1 className="flashcard-title">{ title }</h1>
-                <p className="flashcard-phonetic">{ phonetic }</p>
-                <p className="flashcard-description">{ description }</p>
 
-                <Like 
-                    showDelete={showDelete}
-                    likes={likes}
-                    liked={liked}
-                    onDelete={() => this.onDelete(id)}
-                    onLike={() => this.onLike(id)}/>
+                    <h1 className="flashcard-title">
+                        <span style={{ marginRight: "1vh" }}>{ title }</span>
+                        <FontAwesomeIcon icon={ faVolumeUp } />
+                    </h1>
+                    <p className="flashcard-phonetic">{ phonetic }</p>
+                    <p className="flashcard-description">{ description }</p>
+
+                    <Like 
+                        showDelete={showDelete}
+                        likes={likes}
+                        liked={liked}
+                        onDelete={() => this.onDelete(id)}
+                        onLike={() => this.onLike(id)}/>
             </div>
         )
     }
